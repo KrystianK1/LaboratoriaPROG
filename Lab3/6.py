45c59a1898d19870d1dc87b906d3eeef6a3807d6
w67247 <krystiank@interia.pl>
2022-11-15 18:40:47 +0100

    Lab5

--- Lab3/6.py
+++ Lab3/6.py
@@ -1,11 +1,13 @@
 x = 1
 sum = 0
 numOfStudents = int(input("Wprowadz liczbe studentow:"))
-while x <= numOfStudents:
+while True:
     points = int(input(f"Wprowadz liczbe punktow studenta {x}: "))
     if points < 0 or points > 100:
         continue
     sum += points
     x+=1
+    if x > numOfStudents:
+        break
 median = sum/numOfStudents
 print("Średnia punktów uczniów wynosi:", median)
